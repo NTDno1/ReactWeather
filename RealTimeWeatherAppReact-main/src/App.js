e75c6c60b4a4d2490c55e03fb862fab10b6456d2
dungdahe163646 <dungdahe163646@fpt.edu.vn>
2023-08-23 21:01:28 +0700

    update

---
+++ RealTimeWeatherAppReact-main/src/App.js
@@ -0,0 +1,74 @@
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css"
+import { useEffect, useState } from "react";
+import './App.css';
+
+function App() {
+
+
+  const apiKey = "f56f24967aaf51182d1d4df628297c6d"
+  const [inputCity, setInputCity] = useState("")
+  const [data, setData] = useState({})
+  const [data2, setData2] = useState({})
+
+
+  const getWetherDetails = (cityName) => {
+    if (!cityName) return
+    const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey
+    axios.get(apiURL).then((res) => {
+      console.log("response", res.data)
+      setData(res.data)
+    }).catch((err) => {
+      console.log("err", err)
+    })
+  }
+
+  const getNews = () => {
+    const apiURL = "https://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=ef0e8582866d44e084a0fc7f32a7f4ca"
+    axios.get(apiURL).then((res) => {
+      console.log("thoitiet", res.data)
+      setData2(res.data)
+    }).catch((err) => {
+      console.log("err", err)
+    })
+  }
+
+  const handleChangeInput = (e) => {
+    console.log("value", e.target.value)
+    setInputCity(e.target.value)
+  }
+
+  const handleSearch = () => {
+    getWetherDetails(inputCity)
+    getNews()
+  }
+
+
+  return (
+    <div className="weather">
+      <div>
+          <input type="text" className="form-control"
+            value={inputCity}
+            onChange={handleChangeInput} />
+          <button className="btn btn-primary" type="button"
+            onClick={handleSearch}
+          >Search</button>
+      </div>
+
+      {Object.keys(data).length > 0 &&
+        <div >
+          <div >
+            <img className="weathorIcon"
+              src="https://i.pinimg.com/originals/77/0b/80/770b805d5c99c7931366c2e84e88f251.png" />
+            <h5 className="weathorCity">
+              {data?.name}
+            </h5>
+            <h6 className="weathorTemp">{((data?.main?.temp) - 273.15).toFixed(2)}°C</h6>
+          </div>
+        </div>
+      }
+    </div>
+  );
+}
+
+export default App;
